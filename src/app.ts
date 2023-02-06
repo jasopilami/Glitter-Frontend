7353bcd4b1157deb2eb459bb112acfa53ff218e0
jasopilami <115068582+jasopilami@users.noreply.github.com>
2023-02-06 22:13:40 +0100

    Glitter Frontend

---
+++ src/app.ts
@@ -0,0 +1,53 @@
+function createTweet(tweet) {
+  return `
+        <div class="card-text nb-3 glitter-container bg-color shadow-lg">
+        <div class="card-body m-2 pt-3">
+        <p class="card-text color">${tweet.tweet}</p>
+        <hr />
+        <p class="card-text color">${tweet.user || ""}</p>
+        <hr />
+        <div class="card-footer">
+            <button type="button" class="btn btn-icon color"><i class="fa-solid fa-retweet"></i></button>
+            <button type="button" class="btn btn-icon color"><i class="fa-solid fa-thumbs-up"></i></button>
+       </div>
+      </div>
+    </div>`;
+}
+
+function postGlitt() {
+  const text = document.getElementById("glittText") as HTMLTextAreaElement;
+  const payload = { tweet: text.value, user: "Jascha" };
+  fetch("http://localhost:3000/glitts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((tweet) => {
+      document.getElementById("cards-container").innerHTML +=
+        createTweet(tweet);
+    });
+}
+
+function displayTweets(tweets) {
+  tweets.forEach((element) => {
+    document.getElementById("cards-container").innerHTML +=
+      createTweet(element);
+  });
+}
+
+fetch("http://localhost:3000/glitts")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    displayTweets(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
